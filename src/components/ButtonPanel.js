ee3143a1093728a133038c299b489ca1642f6514
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-05-13 14:09:54 -0500

    Rendering buttonPanel components

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function ButtonPanel() {
+  return (
+    <div className="buttonsPanel">
+      <div>
+        <div>AC</div>
+        <div>+/-</div>
+        <div>%</div>
+        <div>/</div>
+      </div>
+      <div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>*</div>
+      </div>
+      <div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>-</div>
+      </div>
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>+</div>
+      </div>
+      <div>
+        <div>0</div>
+        <div>.</div>
+        <div>=</div>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPanel;
