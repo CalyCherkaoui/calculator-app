abf0b087f6e9e9fa83b9fa7b1fa55fa3468d048a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-05-18 12:38:21 -0500

    Declare App as react class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React from 'react';
+import { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // import calculate from './logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+// const App = () => (
+//   <>
+//     <Display />
+//     <ButtonPanel />
+//   </>
+// );
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
+  };
+};
 export default App;
