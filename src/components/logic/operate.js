86b2c891acbeb114e8aad9eee35571141ce3b884
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-05-21 15:13:21 -0500

    Fix calling Big methode in logic part

--- src/components/logic/operate.js
+++ src/components/logic/operate.js
@@ -2,21 +2,24 @@ import Big from 'big.js';
 
 const operate = (number0ne, numberTwo, operation) => {
   let total = 0;
+  const numOne = new Big(number0ne);
+  const numTwo = new Big(numberTwo);
+
   switch (operation) {
     case '+':
-      total = Big(number0ne) + Big(numberTwo);
+      total = numOne + numTwo;
       break;
     case '-':
-      total = Big(number0ne) - Big(numberTwo);
+      total = numOne - numTwo;
       break;
     case '*':
-      total = Big(number0ne) * Big(numberTwo);
+      total = numOne * numTwo;
       break;
     case '/':
-      total = (numberTwo === 0) ? 'Error' : (Big(number0ne) / Big(numberTwo));
+      total = (numberTwo === 0) ? 'Error' : (numOne / numTwo);
       break;
     case '%':
-      total = Big(number0ne) / 100;
+      total = numOne / 100;
       break;
     default:
       break;
