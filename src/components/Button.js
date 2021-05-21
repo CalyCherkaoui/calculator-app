5240142e6da446db2436b56452cc16ac0db29001
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-05-21 11:56:56 -0500

    Implement clickhandler in button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <p>{ name }</p>
+const Button = ({ name, clickHandler }) => (
+  <>
+    <button className="btn" onClick={() => clickHandler(name)} type="button">
+      { name }
+    </button>
+  </>
 );
 
-Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = { name: PropTypes.string.isRequired, clickHandler: PropTypes.func.isRequired };
 
 export default Button;
