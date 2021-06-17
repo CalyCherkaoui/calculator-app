0d6555830a812094e97e1f67d0daaead68abc4b5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-17 18:05:10 -0500

    Add links route to the navigation bar items

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
     <div className='navbar_wrapper'>
       <div>Math Magicians</div>
       <ul className='nav_list'>
-        <li className='nav_link'>Home</li>
-        <li className='nav_link'>Quote</li>
-        <li className='nav_link'>Calculator</li>
+        <li className='nav_item'><Link to='/' className='nav_link'>Home</Link></li>
+        <li className='nav_item'><Link to='/Quote' className='nav_link'>Quote</Link></li>
+        <li className='nav_item'><Link to='/Calculator' className='nav_link'>Calculator</Link></li>
       </ul>
     </div>
   )
