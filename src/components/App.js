a423591d917916ec0491ef489c13716e33108dc1
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-17 16:06:14 -0500

    Refactor App class compnont into fuction component

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,45 @@
-import './App.css';
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from './logic/calculate';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    /* eslint-disable react/no-unused-state */
-    this.state = {
-      total: '0',
-      next: '',
-      operation: '',
-    };
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     /* eslint-disable react/no-unused-state */
+//     this.state = {
+//       total: '0',
+//       next: '',
+//       operation: '',
+//     };
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick(btn) {
-    this.setState(
-      (state) => {
-        const data = { total: state.total, next: state.next, operation: state.operation };
-        return calculate(data, btn);
-      },
-    );
-  }
+//   handleClick(btn) {
+//     this.setState(
+//       (state) => {
+//         const data = { total: state.total, next: state.next, operation: state.operation };
+//         return calculate(data, btn);
+//       },
+//     );
+//   }
 
-  render() {
-    return (
-      <>
-        <Display result={this.state} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <>
+//         <Display result={this.state} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [state, setState] = useState({ total: '', next: '', operation: '' });
+  return <div>app</div>;
+};
 
 export default App;
