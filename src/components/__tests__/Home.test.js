36ed21e64f201468c3cebd45b01ca99046d36809
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-18 15:01:10 -0500

    Fix home test

--- src/components/__tests__/Home.test.js
+++ src/components/__tests__/Home.test.js
@@ -5,7 +5,7 @@ import Home from '../Home';
 describe(
   'Home page',
   () => {
-    it('Matches Quote page snapshot', () => {
+    it('Matches Home page snapshot', () => {
       const comp = renderer.create(<Home />);
       expect(comp).toMatchSnapshot();
     });
