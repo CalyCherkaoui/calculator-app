af7e582adc96d1568dd4b755e64744344842785c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-18 12:06:21 -0500

    Fix compilation errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // import React, { Component } from 'react';
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from './logic/calculate';
@@ -8,38 +8,6 @@ import './App.css';
 import Home from './Home';
 import Quote from './Quote';
 import Navigation from './Navigation';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     /* eslint-disable react/no-unused-state */
-//     this.state = {
-//       total: '0',
-//       next: '',
-//       operation: '',
-//     };
-
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(btn) {
-//     this.setState(
-//       (state) => {
-//         const data = { total: state.total, next: state.next, operation: state.operation };
-//         return calculate(data, btn);
-//       },
-//     );
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <Display result={this.state} />
-//         <ButtonPanel clickHandler={this.handleClick} />
-//       </>
-//     );
-//   }
-// }
 
 const App = () => {
   const [state, setState] = useState({ total: '', next: '', operation: '' });
@@ -49,30 +17,24 @@ const App = () => {
   };
 
   return (
-    // <>
-      // <div>
-      //   <Display result={`${state.total} ${state.operation} ${state.next}`} />
-      //   <ButtonPanel clickHandler={handleClick} />
-      // </div>
-    // </>
     <Router>
       <>
         <Navigation />
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route 
-            exact 
-            path='/calculator'
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/calculator"
             render={
-                      () => (      
-                              <div>
-                                <Display result={`${state.total} ${state.operation} ${state.next}`} />
-                                <ButtonPanel clickHandler={handleClick} />
-                              </div>
-                            )
+                      () => (
+                        <div>
+                          <Display result={`${state.total} ${state.operation} ${state.next}`} />
+                          <ButtonPanel clickHandler={handleClick} />
+                        </div>
+                      )
                     }
           />
-          <Route exact path='/quotes' component={Quote} />
+          <Route exact path="/quote" component={Quote} />
         </Switch>
       </>
     </Router>
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,39 +6,37 @@ const ButtonPanel = ({ clickHandler }) => {
   const handleClick = (buttonName) => { clickHandler(buttonName); };
 
   return (
-    <>
-      <div className="buttonsPanel">
-        <div className="buttons_row">
-          <Button name="AC" key="AC" clickHandler={handleClick} />
-          <Button name="+/-" clickHandler={handleClick} />
-          <Button name="%" clickHandler={handleClick} />
-          <Button name="/" clickHandler={handleClick} />
-        </div>
-        <div className="buttons_row">
-          <Button name="7" clickHandler={handleClick} />
-          <Button name="8" clickHandler={handleClick} />
-          <Button name="9" clickHandler={handleClick} />
-          <Button name="*" clickHandler={handleClick} />
-        </div>
-        <div className="buttons_row">
-          <Button name="4" clickHandler={handleClick} />
-          <Button name="5" clickHandler={handleClick} />
-          <Button name="6" clickHandler={handleClick} />
-          <Button name="-" clickHandler={handleClick} />
-        </div>
-        <div className="buttons_row">
-          <Button name="1" clickHandler={handleClick} />
-          <Button name="2" clickHandler={handleClick} />
-          <Button name="3" clickHandler={handleClick} />
-          <Button name="+" clickHandler={handleClick} />
-        </div>
-        <div className="buttons_row">
-          <Button name="0" clickHandler={handleClick} />
-          <Button name="." clickHandler={handleClick} />
-          <Button name="=" clickHandler={handleClick} />
-        </div>
+    <div className="buttonsPanel">
+      <div className="buttons_row">
+        <Button name="AC" key="AC" clickHandler={handleClick} />
+        <Button name="+/-" clickHandler={handleClick} />
+        <Button name="%" clickHandler={handleClick} />
+        <Button name="/" clickHandler={handleClick} />
       </div>
-    </>
+      <div className="buttons_row">
+        <Button name="7" clickHandler={handleClick} />
+        <Button name="8" clickHandler={handleClick} />
+        <Button name="9" clickHandler={handleClick} />
+        <Button name="*" clickHandler={handleClick} />
+      </div>
+      <div className="buttons_row">
+        <Button name="4" clickHandler={handleClick} />
+        <Button name="5" clickHandler={handleClick} />
+        <Button name="6" clickHandler={handleClick} />
+        <Button name="-" clickHandler={handleClick} />
+      </div>
+      <div className="buttons_row">
+        <Button name="1" clickHandler={handleClick} />
+        <Button name="2" clickHandler={handleClick} />
+        <Button name="3" clickHandler={handleClick} />
+        <Button name="+" clickHandler={handleClick} />
+      </div>
+      <div className="buttons_row">
+        <Button name="0" clickHandler={handleClick} />
+        <Button name="." clickHandler={handleClick} />
+        <Button name="=" clickHandler={handleClick} />
+      </div>
+    </div>
   );
 };
 
