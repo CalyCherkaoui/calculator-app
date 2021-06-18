d8a2f795f6f338e6acf8e4ffb32a01f1d144f426
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-18 17:09:18 -0500

    Update Test of ButtonPanel component

--- src/components/__tests__/ButtonPanel.test.js
+++ src/components/__tests__/ButtonPanel.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import shallow from 'enzyme';
 import renderer from 'react-test-renderer';
 import ButtonPanel from '../ButtonPanel';
 import '@testing-library/jest-dom';
@@ -8,16 +7,11 @@ const handleClick = () => ({ total: '0', next: '0', operation: '' });
 describe(
   'ButtonPanel component',
   () => {
-    // let component;
-    // beforeEach(() => { component = shallow(<ButtonPanel clickHandler={clickHandler} />); });
     it(
       'rendrs sucessfully compared to its snapshot',
       () => {
         const component = renderer.create(<ButtonPanel clickHandler={handleClick} />);
         expect(component).toMatchSnapshot();
-        // eslint-disable-next-line no-unused-expressions
-        // expect(component.find('.buttonsPanel')).toBeTruthy;
-        // expect(component).toMatchSnapshot();
       },
     );
   },
